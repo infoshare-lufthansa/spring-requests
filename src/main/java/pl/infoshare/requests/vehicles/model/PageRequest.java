1951ef5570c8e8d0e528625ea1dfa4af9e007329
maciej.koziara <maciej.koziara@airhelp.com>
2021-05-21 18:57:22 +0200

    3rd exercise solution

--- src/main/java/pl/infoshare/requests/vehicles/model/PageRequest.java
+++ src/main/java/pl/infoshare/requests/vehicles/model/PageRequest.java
@@ -2,8 +2,25 @@ package pl.infoshare.requests.vehicles.model;
 
 import lombok.Value;
 
+import java.util.Objects;
+
 @Value
 public class PageRequest {
-    Long page = 0L;
-    Long pageSize = Long.MAX_VALUE;
+    Long page;
+    Long pageSize;
+
+    public Long getPage() {
+        if (Objects.isNull(page)) {
+            return 0L;
+        }
+
+        return page;
+    }
+
+    public Long getPageSize() {
+        if (Objects.isNull(pageSize)) {
+            return Long.MAX_VALUE;
+        }
+        return pageSize;
+    }
 }
